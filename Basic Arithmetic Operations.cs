f8a450d367555ef3c0a1749d1a1fe1f05bbac139
Diana Ghabuzyan <63903773+DianaGhabuzyan@users.noreply.github.com>
2020-05-03 19:31:01 +0400

    Add files via upload

---
+++ Basic Arithmetic Operations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Types_and_operations
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = -5;
+            uint b = 5;
+            char c = 'D';
+            float d = 9.9F;
+            double e = 9.9;
+            decimal f = 9.9M;
+            bool g = true;
+            string h = "Bonjour";
+
+            // basic calculator model
+
+            int num1, num2;
+            int add, sub, mul, rem;
+            float div;
+
+            // accepting values from users
+
+            Console.WriteLine("Enter first number\t\t");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number\t\t");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //stating the operations
+
+            add = num1 + num2;
+            sub = num1 + num2;
+            mul = num1 * num2;
+            div = (float)num1 / num2; // cast operator (float) - A cast is a special
+            //operator that forces one data type to be converted into another. A
+            rem = num1 % num2; 
+           
+
+            //printing the output
+
+            Console.WriteLine("Addition\t\t{0}", add);
+            Console.WriteLine("Subtraction\t\t{0}", sub);
+            Console.WriteLine("Multiplication\t\t{0}", mul);
+            Console.WriteLine("Division\t\t{0}", div);
+            Console.WriteLine("Remainder\t\t{0}", rem);
+
+            Console.ReadKey(); 
+
+
+
+        }
+    }
+}
