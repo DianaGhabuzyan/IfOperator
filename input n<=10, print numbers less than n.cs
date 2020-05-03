70c75c6b8f3c2851ae332adc1053b6610f973f10
Diana Ghabuzyan <63903773+DianaGhabuzyan@users.noreply.github.com>
2020-05-03 22:14:45 +0400

    Add files via upload

---
+++ input n<=10, print numbers less than n.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace problem3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            int result;
+            Console.WriteLine("Print the number from 1 to 10");
+            result = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=0; i<result; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
