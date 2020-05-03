84b29b58429557890233bf6f7914f2a74c52b3d5
Diana Ghabuzyan <63903773+DianaGhabuzyan@users.noreply.github.com>
2020-05-03 19:28:41 +0400

    Add files via upload

---
+++ SumofDigits.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace miniproblems
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {  
+
+            int number;
+            int remainder;
+            int sum=0;
+
+            //get the number from the user and convert it
+            Console.WriteLine("Please enter your number");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            //get the remainder of that number to get the last digits
+            while (number > 0)
+            {
+                remainder = number % 10;
+                sum = remainder + sum;
+                //divide by 10 to get the first digit
+                number = number / 10;
+            }
+            
+            Console.WriteLine("Sum of the digits is {0}", sum);
+
+            Console.ReadKey();
+        }
+    }
+}
