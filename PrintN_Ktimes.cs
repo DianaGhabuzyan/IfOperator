56fa57733a6212bcdf923628f13cc2381974a45d
Diana Ghabuzyan <63903773+DianaGhabuzyan@users.noreply.github.com>
2020-05-03 22:50:24 +0400

    Add files via upload

---
+++ PrintN_Ktimes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace N_Ktimes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int resultK;
+            int resultN;
+            int a = 0;
+
+            Console.WriteLine("Write any number(K)");
+            resultK = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Say how many times (N) you want to print it (K)");
+            resultN = Convert.ToInt32(Console.ReadLine());
+
+            while (a < resultN)
+            {
+                Console.WriteLine(resultK);
+                resultN--;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+            
