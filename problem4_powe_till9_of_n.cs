534f566047b6f7ddde1c3eea5b93c953d1077bf6
Diana Ghabuzyan <63903773+DianaGhabuzyan@users.noreply.github.com>
2020-05-04 23:15:22 +0400

    Add files via upload

---
+++ problem4_powe_till9_of_n.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Problem4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int result;
+            Console.WriteLine("Print the number");
+            result = Convert.ToInt32(Console.ReadLine());
+           
+            for (int i=1;i<10;i++)
+            {
+                Console.WriteLine(Math.Pow(result,i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
