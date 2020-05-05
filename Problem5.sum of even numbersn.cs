5afec173e70c34eb400f7396a612b82033960e40
Diana Ghabuzyan <63903773+DianaGhabuzyan@users.noreply.github.com>
2020-05-05 12:01:17 +0400

    Add files via upload

---
+++ Problem5.sum of even numbersn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problem5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int result;
+            int sum = 0;
+            Console.WriteLine("Print the number");
+            result = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < result; i+=2)
+            {
+                sum = sum + i;
+                Console.WriteLine(sum);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
